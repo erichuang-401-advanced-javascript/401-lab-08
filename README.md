02869f4a9ed282b774f494ca16cfbf2bbf168e90
Eric Huang <ejhuang84@gmail.com>
2019-08-23 16:52:48 -0700

    fixed tests

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 401-lab-08
+
+Create a basic CRUD API.
+Use Express' route methods and middleware to dynamically handle all CRUD requests through one route.
+
+ESLint is finding an error in supergoose.js and is not allowing the error to be disabled.
+Travis is not passing.
--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -4,65 +4,155 @@ const server = require('../lib/server');
 const supergoose = require('./supergoose');
 const mockRequest = supergoose(server.app);
 
-describe('the /products route should be able to CRUD it up', () => {
+describe('both /products and /categories routes should be able to CRUD it up', () => {
+
+  let models = [ 'products', 'categories' ];
+
+  models.forEach( model => {
+
+    test('create a new item', () => {
+
+      let bank = [{
+        name : 'test1',
+        price : 0,
+        cool : false,
+      }, {
+        name : 'asdf1',
+        type : 'qwer',
+        rating : 5,
+      }];
+
+      let test;
+
+      if ( model === 'products' ) test = bank[0];
+      else test = bank[1];
+
+      return mockRequest.post(`/${model}`)
+        .send( test )
+        .then( response => {
+          expect(response.status).toEqual(200);
+          expect(response.body.name).toEqual(test.name);
+        });
+    });
   
-  test('create a new item', () => {
-    let test = {
-      name : 'test1',
-      price : 0,
-      cool : false,
+    test('get all items', () => {
+
+      let bank = [{
+        name : 'test2',
+        price : 0,
+        cool : true,
+      },{
+        name : 'asdf2',
+        type : 'poot',
+        rating : 10,
+      }];
+
+      let test;
+
+      if ( model === 'products' ) test = bank[0];
+      else test = bank[1];
+
+      return mockRequest.post(`/${model}`)
+        .send( test )
+        .then (response => {
+          expect(response.status).toEqual(200);
+          expect(response.body.name).toEqual(test.name);
+        })
+        .then( () => {
+          return mockRequest.get(`/${model}`)
+            .send( {} )
+            .then( response => {
+              expect(response.body.count).toEqual(2);
+            });
+        });
+    });
+  
+    test('update an item', () => {
+
+      let bank = [{
+        price : 10,
+      }, {
+        rating : 100,
+      }];
+
+      let test;
+
+      if ( model === 'products' ) test = bank[0];
+      else test = bank[1];
+
+      return mockRequest.get(`/${model}`)
+        .send( {} )
+        .then( response => {
+          let id = response.body.results[0]._id;
+          return mockRequest.put(`/${model}/update?id=${id}`)
+            .send( test )
+            .then( response => {
+              expect(response.status).toEqual(200);
+              expect(response.body.price).toEqual(test.price);
+            });
+        })
+        .then( () => {
+          return mockRequest.get(`/${model}`)
+            .send( {} )
+            .then( response => {
+              expect(response.body.results[0].price).toEqual(test.price);
+            });
+        });
+    });
+  
+    test('delete an item', () => {
+
+      let bank = [{
+        name : 'deleeteemeee',
+        price : 10,
+        cool : false,
+      }, {
+        name : 'deleteme',
+        type : 'asdf',
+        rating: 50,
+      }];
+
+      let test;
+
+      if ( model === 'products' ) test = bank[0];
+      else test = bank[1];
+
+      return mockRequest.post(`/${model}`)
+        .send( test )
+        .then( response => {
+          let id = response.body._id;
+          return mockRequest.put(`/${model}/delete?id=${id}`)
+            .then( () => {
+              return mockRequest.get(`/${model}`)
+                .send(id)
+                .then( response => {
+                  response.body.results.forEach( obj => {
+                    expect(obj._id === id).toEqual(false);
+                  });
+                });
+            });
+        });
+    });
+  });
+});
+
+describe('Error checks on routes work', () => {
+
+  test('500 errors on POST', () => {
+
+    let testbank = { 
+      test1 : { name : 'asdf' },
+      test2 : { name : 'asdf', price : 50 },
+      test3 : { name : 'asdf', price : 50, cool : 'oops' },
     };
-    mockRequest.post('/products')
-      .send( test )
-      .then( response => {
-        expect(response.status).toEqual(201);
-        expect(response.body.name).toEqual('test');
-      });
+
+    Object.keys(testbank).forEach( test => {
+      return mockRequest.post('/products')
+        .send( test )
+        .then( response => {
+          expect(response.status).toEqual(500);
+        });
+    });
   });
 
-  test('get all items', () => {
-    let test = {
-      name : 'test2',
-      price : 0,
-      cool : true,
-    };
-    mockRequest.post('/products')
-      .send( test );
-    mockRequest.get('/products')
-      .send( {} )
-      .then( response => {
-        expect(response.status).toEqual(200);
-        expect(response.body.results.length).toEqual(2);
-      });
-  });
-
-  test('update an item', () => {
-    let test = {
-      price : 10,
-    };
-    mockRequest.get('/products')
-      .send( {} )
-      .then( response => {
-        let id = response.body.results[0]._id;
-        mockRequest.update(`/products/update?${id}`)
-          .send( test )
-          .then( response => {
-            expect(response.status).toEqual(201);
-            expect(response.body.results[0].price).toEqual(10);
-          });
-      });
-  });
-
-  test('delete an item', () => {
-    mockRequest.get('/products')
-      .send( {} )
-      .then( response => {
-        let id = response.body.results[0]._id;
-        mockRequest.delete(`/products/delete?${id}`)
-          .then( response => {
-            console.log(response);
-            expect(response.status).toEqual();
-          });
-      });
-  });
 });
